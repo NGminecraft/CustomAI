3e5bf4cdcf1e6f5ea40604510725ae9f398adeff
NGminecrafter <grahanic001@alpinesd.org>
2025-07-13 15:16:58 -0600

    Maybe need to rewrite POS

--- Semantics/POS/POS.py
+++ Semantics/POS/POS.py
@@ -1,29 +1,52 @@
 import logging
+import os
+import json
 
 LOGGER = logging.getLogger(__name__)
 
 
-def init_pos_tags():
-    import os
-    import json
-    pos_tags = {}
-    dir = os.path.join(os.path.dirname(__file__), 'POS_tags')
-    LOGGER.debug(f'Loading POS tags from {dir}')
-    for file in os.listdir(dir):
-        if file.endswith('.json') and file.startswith('POS_'): # Grabbing only POS jsons
-            with open(os.path.join(dir, file), 'r') as f:
-                data = json.load(f)
-                pos_tags[data["name"]] = data
-                LOGGER.debug(f'Loading POS tag {data["name"]}')
 
-    if len(pos_tags) == 0:
-        LOGGER.warning(f'No POS tags found in {dir}')
-        return {}
-    LOGGER.info(f'Loaded {len(pos_tags)} POS tags from {dir}')
-    return pos_tags
             
 
 
 class POS:
     def __init__(self):
-        self.POS_TAGS = init_pos_tags()
+        self.pos_tags = {}
+        self.group_tags = {}
+        self.part_tags = {}
+        self.known_tags = set()
+        self.known = {}
+
+        self.init_pos_tags()
+    
+    def init_pos_tags(self):
+
+        dir = os.path.join(os.path.dirname(__file__), 'POS_tags')
+        LOGGER.debug(f'Loading POS tags from {dir}')
+
+        for file in os.listdir(dir):
+            if file.endswith('.json'): # Grabbing only jsons
+                with open(os.path.join(dir, file), 'r') as f:
+                    data = json.load(f)
+                    match file:
+                        case f if f.startswith('POS_'):
+                            self.pos_tags[data["name"]] = data["rules"]
+                            LOGGER.debug(f'Loading POS tag {data["name"]}')
+                        
+                        case f if f.startswith('PART_'):
+                            self.part_tags[data["name"]] = data
+                            LOGGER.debug(f"Loaded PART {data['name']}")
+                        
+                        case f if f.startswith('GROUP_'):
+                            self.group_tags[data["name"]] = data
+                            LOGGER.debug(f"Loaded GROUP {data['name']} (containing {data["contains"]})")
+                    
+
+        if len(self.pos_tags) == 0:
+            LOGGER.warning(f'No POS tags found in {dir}')
+            
+        LOGGER.info(f'Loaded {len(self.pos_tags)} POS tags, {len(self.group_tags)} group tags, and {len(self.part_tags)} part tags from {dir}')
+
+    def parse_sentence(self, sentence: list[str]):
+        if len(sentence) == 1 and sentence[0] not in self.known_tags:
+            :
